0b4714156c70dc76866da59f0bf6979f853dfc5d
Thang cg <duongsag.hp11@gmail.com>
2022-06-03 09:20:42 +0700

    updtae

--- QuadraticEquation.ts
+++ QuadraticEquation.ts
@@ -20,21 +20,27 @@ class QuadraticEquation{
     getRoot3() {
         return x1 = x2 = -this.b / this.a;
     }
+    getResult(){
+        if(delta == 0){
+            console.log('Phuong trinh co nghiem kep x1 = x2 = ' + quadraticEquation.getRoot3());
+        }
+        else if(delta >= 0){
+            console.log('Phuong trinh co nghiem thu nhat x1 = ' + quadraticEquation.getRoot1())
+            console.log('Phuong trinh co nghiem thu nhat x2 = ' + quadraticEquation.getRoot2())
+        }
+        else {
+            console.log('The equation has no roots')
+        }
+    }
 }
 let delta;
 let x1,x2;
 let quadraticEquation = new QuadraticEquation(2,6,2)
+
 console.log('delta = ' + quadraticEquation.getDiscriminant());
+console.log(quadraticEquation.getResult())
 
 
-if(delta == 0){
-    console.log('Phuong trinh co nghiem kep x1 = x2 = ' + quadraticEquation.getRoot3());
-}
-else if(delta >= 0){
-    console.log('Phuong trinh co nghiem thu nhat x1 = ' + quadraticEquation.getRoot1())
-    console.log('Phuong trinh co nghiem thu nhat x2 = ' + quadraticEquation.getRoot2())
-}
-else {
-    console.log('The equation has no roots')
-}
+
+
 
